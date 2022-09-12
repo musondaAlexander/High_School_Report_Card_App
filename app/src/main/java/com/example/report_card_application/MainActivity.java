ec13792455230581abb3e4647c7f743e9c4810b4
musondaAlexander <musondaalexander97@gmail.com>
2022-09-12 11:37:40 +0200

    Created Project

---
+++ app/src/main/java/com/example/report_card_application/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.report_card_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
